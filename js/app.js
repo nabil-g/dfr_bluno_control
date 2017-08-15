dd924e485a6eef93c8a91e4bc6ead46ea35daac7
Unknown <nabil.ghedjati@gmail.com>
2017-08-15 23:42:24 +0200

    added fmwks & started to work on the vue instanc

---
+++ js/app.js
@@ -0,0 +1,15 @@
+var app = new Vue({
+  el: "#app",
+  data:{
+    DFRBLU_SERVICE_UUID: '0000dfb0-0000-1000-8000-00805f9b34fb',
+    DFRBLU_CHAR_RXTX_UUID: '0000dfb1-0000-1000-8000-00805f9b34fb',
+    DFRBLU_TX_UUID_DESCRIPTOR: '00002902-0000-1000-8000-00805f9b34fb',
+    devices: {},
+    device: null,
+    connected: false,
+    scanning: false
+  },
+  methods: {
+
+  }
+});
